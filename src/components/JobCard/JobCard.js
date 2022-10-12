f1d122637029b1a4297529275645a0af315d3f05
virusbent <virusbent@gmail.com>
2022-10-12 12:58:09 +0300

    Created initial JobCard component template

---
+++ src/components/JobCard/JobCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './JobCard.css';
+import './JobCardResponsive.css';
+
+export default function JobCard (props) {
+    return (
+        <div className="job-card">
+            <div className="header"></div>
+            <div className="body"></div>
+        </div>
+    )
+}
