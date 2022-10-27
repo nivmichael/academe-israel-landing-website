45437dca7f682bf44a6083d214a1c6a82739acc5
virusbent <virusbent@gmail.com>
2022-10-27 11:11:27 +0300

    Updated utils.js
    Added isRtl method to detect if given text values are right-to-left direction language

--- src/utils.js
+++ src/utils.js
@@ -52,3 +52,10 @@ export const formatExploreCompanyUrlUtil = (siteName, companyId, sponsorshipId) 
 
     return url;
 }
+
+export const isRtl = (text) => {
+    let rtlChars        = '\u0591-\u07FF\u200F\u202B\u202E\uFB1D-\uFDFD\uFE70-\uFEFC';
+    let rtlDirCheck     = new RegExp('^[^'+rtlChars+']*?['+rtlChars+']');
+
+    return rtlDirCheck.test(text);
+}
